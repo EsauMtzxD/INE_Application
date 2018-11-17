71a77b242e07bd9188a36f799f978ea284b029e9
FlyKey10 <43350586+FlyKey10@users.noreply.github.com>
2018-11-17 02:15:55 -0600

    Update 17-11-2018 Graficas estaticas en Index y Vista para el manejo de Excepciones

--- INE_Application/WebApp/Controllers/HomeController.cs
+++ INE_Application/WebApp/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Highsoft.Web.Mvc.Charts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
@@ -10,19 +12,33 @@ namespace WebApp.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }
+            try
+            {
+                List<PieSeriesData> pieData = new List<PieSeriesData>();
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+                pieData.Add(new PieSeriesData() { Name = "PRI", Y = 10.00, Color = "#04B404" });
+                pieData.Add(new PieSeriesData() { Name = "PAN", Y = 10.00, Color = "#2E64FE" });
+                pieData.Add(new PieSeriesData() { Name = "PRD", Y = 10.00, Color = "#FFFF00" });
+                pieData.Add(new PieSeriesData() { Name = "PT", Y = 10.00, Color = "#FF0000" });
+                pieData.Add(new PieSeriesData() { Name = "Partido Verde", Y = 10.00, Color = "#64FE2E" });
+                pieData.Add(new PieSeriesData() { Name = "Independiente", Y = 10.00, Color = "#8904B1" });
 
-            return View();
-        }
+                ViewData["PartyPie"] = pieData;
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+                List<ColumnSeriesData> columnData = new List<ColumnSeriesData>();
+
+                columnData.Add(new ColumnSeriesData() { Name = "Andres Manuel Lopez Obrador", Y = 10.00, Color = "#FF0000" });
+                columnData.Add(new ColumnSeriesData() { Name = "Jose Antonio Meade Kuribreña", Y = 10.00, Color = "#04B404" });
+                columnData.Add(new ColumnSeriesData() { Name = "Jaime Heliodoro Rodriguez Calderon", Y = 10.00, Color = "#8904B1" });
+                columnData.Add(new ColumnSeriesData() { Name = "Ricardo Anaya Cortés", Y = 10.00, Color = "#2E64FE" });
+                columnData.Add(new ColumnSeriesData() { Name = "Margarita Ester Zavala Gómez del Campo", Y = 10.00, Color = "#64FE2E" });
+
+                ViewData["ColumnCandidate"] = columnData;
+            }
+            catch (Exception e)
+            {
+                throw new Excepcion(e.Message);
+            }
 
             return View();
         }
---
+++ INE_Application/WebApp/Models/Excepcion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class Excepcion : Exception
+    {
+        public Excepcion(String msgError) : base(msgError)
+        {
+        }
+    }
+}
